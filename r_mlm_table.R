7b6aa8c449bd38c2a709ad8cf02daae8ee799598
stefanleach <62575738+stefanleach@users.noreply.github.com>
2020-08-18 11:49:26 +0100

    Update r_mlm_table.R

--- r_mlm_table.R
+++ r_mlm_table.R
@@ -22,11 +22,11 @@ tablelmer <- function(x) { # x = test results
   
   #Round
   tidytable[, 4:9] <- tidytable[, 4:9] %>% round(digits=2)
-  tidytable[, 10] <- tidytable[, 10] %>% round(digits=3)
   
   #Tidy p values
   for (i in nrow(tidytable[tidytable$group=="fixed", ])) {
     if(tidytable$p.value[i] < 0.001) {tidytable$p.value[i] <- "< .001"}
+    if(tidytable$p.value[i] > 0.001) {tidytable$p.value[i] <- f_num(tidytable$p.value[i], 3)}
   }
   
   return(tidytable) }
