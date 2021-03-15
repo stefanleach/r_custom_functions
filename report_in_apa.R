ffb5479af07e9c3444a94ae4eacf70e1ac5cd339
stefanleach <62575738+stefanleach@users.noreply.github.com>
2021-03-15 12:40:55 +0000

    Update report_in_apa.R

--- report_in_apa.R
+++ report_in_apa.R
@@ -93,6 +93,33 @@ report_in_apa <-
       }
       return(table)
     }
+    if(grepl(class(x)[1], "metacor")) {
+      require(meta)
+      require(numform)
+      require(tidyverse)
+      table <- 
+        data.frame(effect = c("fixed", "random"),
+                   string = NA)
+      
+      fixed_r <- x$TE.fixed %>% round(2) %>% formatC(digits=2, format='f')
+      fixed_lower <- x$lower.fixed %>% round(2) %>% formatC(digits=2, format='f')
+      fixed_upper <- x$upper.fixed %>% round(2) %>% formatC(digits=2, format='f')
+      fixed_z <- x$statistic.fixed %>% round(2) %>% formatC(digits=2, format='f')
+      if(x$pval.fixed > .001) {fixed_p <- paste("p = ", x$pval.fixed %>% f_num(3), sep = "")}
+      if(x$pval.fixed < .001) {fixed_p <- "p < .001"}
+      
+      random_r <- x$TE.random %>% round(2) %>% formatC(digits=2, format='f')
+      random_lower <- x$lower.random %>% round(2) %>% formatC(digits=2, format='f')
+      random_upper <- x$upper.random %>% round(2) %>% formatC(digits=2, format='f')
+      random_z <- x$statistic.random %>% round(2) %>% formatC(digits=2, format='f')
+      if(x$pval.random > .001) {random_p <- paste("p = ", x$pval.random %>% f_num(3), sep = "")}
+      if(x$pval.random < .001) {random_p <- "p < .001"}
+      
+      table$string[1] <- paste("r = ", fixed_r, ", 95% CI [", fixed_lower, ", ", fixed_upper, "], Z = ", fixed_z, ", ", fixed_p, sep = "")
+      table$string[2] <- paste("r = ", random_r, ", 95% CI [", random_lower, ", ", random_upper, "], Z = ", random_z, ", ", random_p, sep = "")
+      
+      return(table)
+     }
     if(grepl(class(x)[1], "metacont") | 
        grepl(class(x)[1], "metamean")) {
       require(meta)
